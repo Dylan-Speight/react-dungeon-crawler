1c0d12a1d82ae307719d8a482178fc0ccc33a3fd
Dylan-Speight <d.speight92@gmail.com>
2019-11-07 15:51:26 +1000

    initial commit

---
+++ src/features/map/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Map(props) {
+    return (
+        <div
+            style={{
+                position: 'relative',
+                top: '0px',
+                left: '0px',
+                width: '800px',
+                height: '400px',
+                backgroundColor: 'green',
+                border: '4px solid white',
+            }}
+        />
+    )
+}
