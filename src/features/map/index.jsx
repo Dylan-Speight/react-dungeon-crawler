11bb941f780033b4c7598c600487d63b11b3d6cb
Dylan-Speight <d.speight92@gmail.com>
2019-11-16 11:34:45 +1000

    quicksave

--- src/features/map/index.jsx
+++ src/features/map/index.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React from 'react';
+
+function MapTile(props) {
+	return <div></div>;
+}
+
+function MapRow(props) {
+	return props.tiles.map(tile => <MapTile value={tile} />);
+}
 
 export default function Map(props) {
-  return (
-    <div
-      style={{
-        position: "relative",
-        top: "0px",
-        left: "0px",
-        width: "800px",
-        height: "400px",
-        backgroundColor: "green",
-        border: "4px solid white"
-      }}
-    />
-  );
+	return (
+		<div
+			style={{
+				position: 'relative',
+				top: '0px',
+				left: '0px',
+				width: '800px',
+				height: '400px',
+				backgroundColor: 'green',
+				border: '4px solid white',
+			}}>
+			{props.tiles.map(row => (
+				<MapRow tiles={row} />
+			))}
+		</div>
+	);
 }
